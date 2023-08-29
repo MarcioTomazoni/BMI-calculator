4f2fdce1952d9712908658854b0d5421e899a731
Marcio <marciotz13@gmail.com>
2023-08-29 09:59:42 -0300

    First commit
    Upload of project BMI Calculator

---
+++ README.md
@@ -0,0 +1,18 @@
+# BMI Calculator
+This project belongs to "The Complete Android 13 App Development Bootcamp" course, which was created by Mr. Vin Norman and acquired by me through Udemy. [Watch on Udemy](https://www.udemy.com/course/the-complete-android-developer-bootcamp/)
+
+My only intention with publishing the project is to learn how to use the tools: Git, GitHub, as well as the tools used during the course, which by the way is great. I recommend it.
+
+The project creates an app that calculates the Body Mass Index. The app must be installed on a mobile device using Android OS.
+
+The project is developed in java. I'm currently using the API:
+```
+Android Studio Giraffe | 2022.3.1 Patch 1
+Build #AI-223.8836.35.2231.10671973, built on August 17, 2023
+Runtime version: 17.0.6+0-b2043.56-10027231 amd64
+VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
+Windows 10 10.0
+GC: G1 Young Generation, G1 Old Generation
+Memory: 1280M
+Colors: 8
+```
---
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Class Variables; also are called 'Fields'
+    private TextView resultText;
+    private Button calculateButton;
+    private RadioButton maleButton;
+    private RadioButton femaleButton;
+    private EditText ageEditText;
+    private EditText feetEditText;
+    private EditText inchesEditText;
+    private EditText wheightEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        setupButtonClickListener();
+    }
+
+    private void findViews() {
+        resultText = findViewById(R.id.text_view_result);
+        maleButton = findViewById(R.id.radio_button_male);
+        femaleButton = findViewById(R.id.radio_button_female);
+        ageEditText = findViewById(R.id.edit_text_age);
+        feetEditText = findViewById(R.id.edit_text_feet);
+        inchesEditText = findViewById(R.id.edit_text_inches);
+        wheightEditText = findViewById(R.id.edit_text_weight);
+        calculateButton = findViewById(R.id.button_calculate);
+    }
+
+    private void setupButtonClickListener() {
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculateBmi();
+            }
+        });
+    }
+
+    private void calculateBmi() {
+
+    }
+
+}
